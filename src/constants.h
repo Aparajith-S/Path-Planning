5cbe5e9992ac2e71da5aa882eaed33b3b1501f38
Aparajith-S <s.aparajith@live.com>
2021-06-13 05:27:17 +0200

    feat: add more constants for trajectory gen and cost

--- src/constants.h
+++ src/constants.h
@@ -6,33 +6,50 @@
 /// 
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
-namespace path_planner {
+#include <cstdint>
 namespace params {
-
-constexpr float kVehicleRadius = 1.5
-//Mu and Sigma values
+constexpr bool kUseJMT = true;
+constexpr double kMapEnd = 6945.554;
+constexpr std::uint8_t kMaxNrLanes = 3U; 
+constexpr std::uint8_t kLeft = 0U;
+constexpr std::uint8_t kCenter = 1U;
+constexpr std::uint8_t kRight = 2U;
+constexpr float kVehicleRadius = 1.5;
 constexpr int N = 10;
-const double SIGMA_S[3] = { 10.0, 4.0, 2.0 }; // s, s_dot, s_double_dot
-constexpr double SIGMA_D[3] = {1.0, 1.0, 1.0};// d, d_dot, d_double_dot
-constexpr double SIGMA_T = 2.0;
+constexpr int kMaxPoints=50;
+constexpr int kMaxPointsReused=5;
 
+constexpr double kWeightEfficiency = 1.0;
+constexpr double kWeightLegal = 1.0;
+constexpr double kWeightComfort = 1.0;
+constexpr double kWeightCollision = 10000.0;
 //define jerk params
-constexpr double kMaxJerk = 1.0; ///>[m/s³];
+constexpr double kMaxJerk = 10.0; ///>[m/sï¿½];
 constexpr double kJerkInOneSec = 2.0;
 
-constexpr double kMaxHwySpeedLimit = 130.0/3.6; //130[km/h] max speed.
+constexpr double kMaxHwySpeedLimit = 49.0; //49[mi/h] max speed.
+constexpr double kMaxHwySpeedLimitms = 22.0; //49[mi/h] max speed.
 //define max and min comfort acceleration params
-constexpr double kComfortAcc = 2.5; ///>[m/s²];
-constexpr double kComfortDec = -3.5;///>[m/s²];
+constexpr double kComfortAcc = 10; ///>[m/sï¿½];
+constexpr double kComfortDec = 10;///>[m/sï¿½];
+constexpr double kMaxAcc = 10.0; ///>[m/sï¿½];
+constexpr double kTotalAcc = 10.0; ///>[m/sï¿½];
 constexpr double kAccInOneSec = 1.0;
-
 //define max emergency deceleration params
-constexpr double kEmergencyDec = -7.0; ///>[m/s/s];
+constexpr double kEmergencyDec = 7.0; ///>[m/s/s];
 
 //define step
-constexpr float dT = 0.2F;
+constexpr float dT = 0.02F;
+constexpr double kMaxSpeedIncrement = kComfortAcc*dT;
+constexpr double kLaneWidth=4.0;
 
-}
+constexpr double kPredictionHorizon = 70.0;
+constexpr double kSafeProximity = 25.0;
+constexpr double kSafeProximityLC = 10.0;
+
+// object properties
+constexpr double kLengthBoundingBox = 4;
+constexpr double kWidthBoundingBox = 2;
 }
     
 #endif // !CONSTANTS_H
