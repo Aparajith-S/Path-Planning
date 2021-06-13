4cde0cbb4cfb48a38f643781b1573addbadec2de
Aparajith-S <s.aparajith@live.com>
2021-06-13 05:26:53 +0200

    refactor: delete file. move polyfunction to a header only impl.

--- src/polyfunction.h
+++ src/polyfunction.h
@@ -1,25 +1,30 @@
-/// \file
-/// \brief
+/// \file polyfunction.h
+/// \brief polynomial class that computes n-order differential calculus for a given polynomial
 /// \author s.aparajith@live.com
+/// \date 31.05.2021
 /// \copyright None reserved. MIT license
-/// 
-/// 
 #ifndef POLY_FUNCTION_H
 #define POLY_FUNCTION_H
-#include"trajectory.hpp"
+#include"types.h"
+#include"interface.h"
+#include<vector>
 namespace path_planner {
+using interfaces::coefficients;
+
 ///  @class polyfunction class.
 class function
 {
 public:
     explicit function() :
         m_time(-1.0),
-        m_coeffs(vector<double>())
+        m_coeffs(coefficients())
     {}
     virtual ~function() {}
     function(const function&) = default;
     function& operator=(const function&) = default;
-
+    
+    /// @brief compute value of a polynomial at a given time
+    /// @returns function(t) 
     double operator()(double time)
     {
         this->m_time = time;
@@ -41,15 +46,20 @@ public:
         }
         return result;
     }
+
+    /// @brief constructs the polynomial equation
     void constructEquation(const coefficients& coeffs)
     {
         this->m_coeffs = coeffs;
     }
 
+    /// @brief differentiate a polynomial equation
+    /// @details if a coeff is not passed then the function will use its own coefficients to differentiate
+    /// @return function that is differentiated
     function differentiate(const coefficients& coeffs = coefficients())
     {
         function func;
-        coefficients newCoeff = vector<double>();
+        coefficients newCoeff = coefficients();
         const coefficients* oldCoeff;
         if (coeffs.size() == 0)
         {
@@ -73,7 +83,10 @@ public:
         func.constructEquation(newCoeff);
         return func;
     }
-    //returns the function and its N derivatives. default returns vector of {f, fdot,fdotdot}
+
+    /// @brief returns the function and its N derivatives. default returns vector of {f, fdot,fdotdot}
+    /// @details if a coeff is not passed then the function will use its own coefficients to differentiate
+    /// @return vector of N differentiated functions. default order 3 to 1.
     std::vector<function> differentiateNtimes(const coefficients& i_coeffs = coefficients(), const std::uint8_t i_order = 3)
     {
         function func;
@@ -95,8 +108,8 @@ public:
         return returnValue;
     }
 private:
-    double m_time;
-    coefficients m_coeffs;
+    double m_time; // store the last time the coefficent was used to compute. not really needed.
+    coefficients m_coeffs; // internal coefficients for this polynomial.
 };
 }
 #endif
